adcf0e18a19cf41ec4b0fc506c483adcfd89faed
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-05-05 12:12:16 +0200

    upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bild_01",
+      "name": "Bild_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.9315603733163975,
+        "pitch": 0.02767632822846977,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7093837248126,
+          "pitch": 0.17491917903046428,
+          "rotation": 0,
+          "target": "1-bild_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bild_02",
+      "name": "Bild_02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.13351853785590606,
+        "pitch": 0.029653208816217358,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.714295489317484,
+          "pitch": 0.14673174235338138,
+          "rotation": 0,
+          "target": "0-bild_01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Linz269_Dachsanierung_260505",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
